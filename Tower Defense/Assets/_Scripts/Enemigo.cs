dff7c44651c6d8a05ea40fa91e06bfbc6b031914
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2025-02-18 16:25:05 -0600

    Requerimientos de la tarea
    Se agrego todo lo requerido para la tarea del modulo, lo cual es lo siguiente:
    - Se le agrego el "NavMeshAgent" al enemigo en escena para hacer pruebas.
    - Se modifico el script de "Boss" para que este tenga un objetivo y detecte el navmesh del objeto para asi poder recorrer la ruta hasta este.
    - Se volvieron a importar las animaciones y se configuraron para un rig humanoide.
    - Se le aplicaron al modelo del boss.
    - Se programo la mecanica basica del objetivo, ahora puede ser dañado por los enemigos.
    - El jefe ahora puede dañar al objetivo.
    - Se creo una carpeta dedicada a los controllers de animaciones para los NPCs
    - Se movio el controlador del jefe a esta nueva carpeta.
    - Se añadio la animacion de morder al proyecto.
    - La misma logica implementada para el jefe se añadio a el enemigo comun y es ligeramente mas reutilizable debido a que permite modificar el daño que inflinge el enemigo al objetivo.
    - Se agrego un 2ndo enemigo basico al prototipo para que se vea mas lleno.

---
+++ Tower Defense/Assets/_Scripts/Enemigo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+    public GameObject objetivo;
+    public int vida = 100;
+    [Tooltip("Valor utilizado para modificar el daño de los enemigos desde el editor.")]
+    public int damage = 10;
+
+    public Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(objetivo.transform.position);
+        Anim = GetComponent<Animator>();
+        Anim.SetBool("isMoving", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Objetivo")
+        {
+            Anim.SetBool("isMoving", false);
+            Anim.SetTrigger("OnObjectiveReached");
+        }
+    }
+    public void Danar()
+    {
+        objetivo?.GetComponent<Objetivo>().RecibirDano(damage);
+    }
+    public void RecibirDano(int dano = 5)
+    {
+        vida -= dano;
+    }
+}
